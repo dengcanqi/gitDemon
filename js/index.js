cc78e2684819595a20a53759f3bb23ab603a84ff
deng <dengcanqi@163.com>
2019-06-10 16:47:02 +0800

    初始化项目

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", function () {
+    var btn = document.querySelector("#btn");
+    var box = document.querySelector(".box");
+    btn.addEventListener("click", function () {
+        // console.log(111);
+        box.style.backgroundColor="skyblue"
+    });
+})
