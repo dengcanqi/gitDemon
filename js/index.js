c5084ea69cf1f2bd5a5d63f8c51f54dea8af5fc1
deng <dengcanqi@163.com>
2019-06-10 18:41:57 +0800

    添加按钮，点击按钮改变按钮自身颜色

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 window.addEventListener("load", function () {
     var btn = document.querySelector("#btn");
+    var btn01 = document.querySelector("#btn01");
     var box = document.querySelector(".box");
     btn.addEventListener("click", function () {
         // console.log(111);
-        box.style.backgroundColor="skyblue"
-        box.style.width="500px"
-        box.style.height="500px"
+        box.style.backgroundColor = "skyblue"
+        box.style.width = "500px"
+        box.style.height = "500px"
     });
+    btn01.addEventListener('click', function () {
+        this.classList.add('btnColor')
+    })
 })
